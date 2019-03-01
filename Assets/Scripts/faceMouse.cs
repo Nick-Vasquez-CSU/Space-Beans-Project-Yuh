11ab1d411987f271034c98ddeb1c599f35c865e3
Lewis Lam <ktrinity@csu.fullerton.edu>
2019-03-01 14:17:21 -0800

    Updated faceMouse

--- Assets/Scripts/faceMouse.cs
+++ Assets/Scripts/faceMouse.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class faceMouse : MonoBehaviour
 {
+    public float speed = 5f;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
     }
 
 
