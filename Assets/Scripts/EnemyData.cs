cb016bfc1801caf05ff65849c3bc0df689fbc6f2
Nicolas Vasquez <nvasquez682@gmail.com>
2019-03-08 15:25:39 -0800

    Enemy + Bullets
    Added enemy skeleton code
    Started bullet code

--- Assets/Scripts/EnemyData.cs
+++ Assets/Scripts/EnemyData.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class EnemyData : MonoBehaviour
 {
-    // Start is called before the first frame update
+	private int Health;
+	public int Enemydiff, Enemytype;
+	public GameObject self;
     void Start()
     {
-        
+		BaseStats();
     }
 
-    // Update is called once per frame
+    void BaseStats()
+	{
+		if (Enemydiff == 1)
+		{
+			Health = 10;
+		}
+		else if (Enemydiff == 2)
+		{
+			Health = 20;
+		}
+		else if (Enemydiff == 3)
+		{
+			Health = 30;
+		}
+		else
+		{
+			Debug.Log("Invalid Difficulty lulw");
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Bullet_type1"))
+		{
+			Health -= 10;
+			other.gameObject.SetActive(false);
+		}
+
+	}
+
+	void UpdatedStats()
+	{
+		if (Health <= 0)
+		{
+			self.SetActive(false);
+		}
+	}
     void Update()
     {
-        
+		UpdatedStats();
     }
 }
