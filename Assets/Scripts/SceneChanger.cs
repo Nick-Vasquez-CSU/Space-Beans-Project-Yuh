2eafbb9519c1d6732f0ac9993797eb46af632261
Axnon <tori09876@gmail.com>
2019-03-22 01:59:58 -0700

    Main Menu links to temp level 1
    Main Menu works for the play button and maybe quit button. Temp level 1 is an open space allowing player to roam within a small box and collect the can of bean.

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Level 1_temp");
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene("Credits_Scene");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
