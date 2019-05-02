09b132c199a0974c40b37b5991d9a841d34912ad
Lewis Lam <lamlewis99@gmail.com>
2019-05-01 23:03:59 -0700

    Added some animations for char

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,12 +7,15 @@ public class CameraFollow : MonoBehaviour
 	public float speed;
 	private float regularSpeed;
 	private Rigidbody2D rb2d;
+    public Animator animator;
+    SpriteRenderer sr;
 
     void Start()
     {
 		regularSpeed = 17;
 		speed = regularSpeed;
-		rb2d = GetComponent<Rigidbody2D>();   
+		rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
 	void Movement()
@@ -22,22 +25,36 @@ public class CameraFollow : MonoBehaviour
 			speed = 34;
 		}
 
-		if (Input.GetKey(KeyCode.D))
-		{
-			rb2d.velocity = new Vector2(speed, 0);
-		}
-		if (Input.GetKey(KeyCode.A))
-		{
-			rb2d.velocity = new Vector2(-speed, 0);
-		}
-		if (Input.GetKey(KeyCode.W))
-		{
-			rb2d.velocity = new Vector2(0, speed);
-		}
-		if (Input.GetKey(KeyCode.S))
-		{
-			rb2d.velocity = new Vector2(0, -speed);
-		}
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.velocity = new Vector2(speed, 0);
+            animator.SetBool("isMoving", true);
+            sr.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.velocity = new Vector2(-speed, 0);
+            animator.SetBool("isMoving", true);
+            sr.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            rb2d.velocity = new Vector2(0, speed);
+            animator.SetBool("isMoving", true);
+            animator.SetBool("isMovingDown", false);
+            animator.SetBool("isMovingUp", true);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb2d.velocity = new Vector2(0, -speed);
+            animator.SetBool("isMoving", true);
+            animator.SetBool("isMovingUp", false);
+            animator.SetBool("isMovingDown", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
 	}
 	
     void FixedUpdate()
--- Assets/Scripts/enemyFacing.cs
+++ Assets/Scripts/enemyFacing.cs
@@ -6,22 +6,19 @@ public class enemyFacing : MonoBehaviour
 {
     public bool isFacingRight = true;
     SpriteRenderer sr;
-    Transform enemyPos;
-    Transform playerPos;
+    public GameObject enemyPos;
+    public GameObject playerPos;
 
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        enemyPos = GameObject.Find("enemy_2").transform;
-        playerPos = GameObject.Find("Test_Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        isFacingRight = enemyPos.position.x < playerPos.position.x ? true : false;
+        isFacingRight = enemyPos.transform.position.x < playerPos.transform.position.x ? true : false;
         sr.flipX = (isFacingRight == false) ? true : false;
-        Debug.Log("Enemy Player Position: " + enemyPos.position.x + "Player Position: " + playerPos.position.x);
     }
 }
